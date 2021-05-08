56ac54e5c4ba387cf7fb5fc67960eb42ca87136a
Stuart Thompson <stuart@stuartthompson.net>
2021-05-07 20:41:19 -0700

    Bars: Render status bars via trait.

--- src/bars.rs
+++ src/bars.rs
@@ -1,5 +1,13 @@
+use crate::CoreData;
+
 mod editor_info_bar;
 mod status_bar;
 
 pub use editor_info_bar::EditorInfoBar;
 pub use status_bar::StatusBar;
+
+/// Bars render status information
+pub trait Bar {
+    /// Renders the bar
+    fn render(&self, core_data: &CoreData) -> String;
+}
