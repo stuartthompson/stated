7d474088c2c6a6099f0e43bb375ce140f618ff52
Stuart Thompson <stuart@stuartthompson.net>
2021-07-29 16:47:18 -0700

    Editor: Isolating document editor struct.
    This moves dimension information about an editor away from core data and into the editor instances.

--- src/core_data.rs
+++ src/core_data.rs
@@ -1,23 +1,17 @@
 use std::time::{Duration, SystemTime};
 
-pub struct CoreData<'a> {
-    cols: u16,
-    rows: u16,
-    file_path: &'a str,
+pub struct CoreData {
     start_time: SystemTime,
     running_for_secs: u64,
     frames: u64
 }
 
-impl<'a> CoreData<'a> {
-    pub fn new() -> CoreData<'a> {
+impl CoreData {
+    pub fn new() -> CoreData {
         CoreData {
             start_time: SystemTime::now(),
             running_for_secs: 0,
             frames: 0,
-            cols: 0,
-            rows: 0,
-            file_path: "",
         }
     }
 
@@ -28,36 +22,19 @@ impl<'a> CoreData<'a> {
         self.frames += 1;
     }
 
-    pub fn get_frames(&self) -> u64 {
+    pub fn frames(&self) -> u64 {
         self.frames
     }
 
-    pub fn get_uptime(&self) -> u64 {
+    pub fn uptime(&self) -> u64 {
         self.running_for_secs
     }
 
-    pub fn get_fps(&self) -> u64 {
+    pub fn fps(&self) -> u64 {
         if self.running_for_secs == 0 {
             return 0;
         } else {
             return self.frames / self.running_for_secs;
         }
-    }
-
-    pub fn set_dimensions(&mut self, cols: u16, rows: u16) {
-        self.cols = cols;
-        self.rows = rows;
-    }
-
-    pub fn set_file_path(&mut self, file_path: &'a str) {
-        self.file_path = file_path;
-    }
-
-    pub fn get_dimensions(&self) -> (u16, u16) {
-        return (self.cols, self.rows);
-    }
-
-    pub fn get_file_path(&self) -> &str {
-        self.file_path
     }
 }
