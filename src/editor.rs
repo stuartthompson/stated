508c6514ed7a1a471eb8d3bc46aba0639ca839c7
Stuart Thompson <stuart@stuartthompson.net>
2021-07-29 17:34:06 -0700

    Editor Dimensions and Cursor Movement
    An editor now takes a dimensions object upon construction. The editor now also constrains movement to the render boundaries appropriately. Adds unit tests to that effect.

--- src/editor.rs
+++ src/editor.rs
@@ -1,12 +1,13 @@
+mod dimensions;
+
+pub use dimensions::Dimensions;
+
 /// An editor hosts a single open document. The program itself may have many
 ///   open editors. Each editor is given a different portion of the screen into
 ///   which it can render its content.
 pub struct Editor {
-    /// The number of columns this editor is currently allocated for display.
-    pub columns: u16,
-
-    /// The number of rows this editor is currently allocated for display.
-    pub rows: u16,
+    /// The dimensions allocated to this editor to use to display its contents.
+    pub dimensions: Dimensions,
     
     /// The index of the column where the cursor is currently located.
     ///
@@ -20,17 +21,27 @@ pub struct Editor {
 }
 
 impl Editor {
-    pub fn new() -> Editor {
+    /// Returns a new Editor.
+    /// 
+    /// # Arguments
+    /// 
+    /// * `columns` - The number of
+    pub fn new(dimensions: Dimensions) -> Editor {
         Editor {
-            columns: 0,
-            rows: 0,
+            dimensions: dimensions,
             column_ix: 0,
             row_ix: 0,
         }
-    }   
-    pub fn resize(&mut self, columns: u16, rows: u16) {
-        self.columns = columns;
-        self.rows = rows;
+    }
+
+    /// Resizes the render area for an editor.
+    ///
+    /// # Arguments
+    /// 
+    /// * `self` - The editor being resized.
+    /// * `dimensions` - The dimensions describing the updated render area.
+    pub fn resize(&mut self, dimensions: Dimensions) {
+        self.dimensions = dimensions;
     }
 
     /// Moves the cursor to the left a specified number of columns.
@@ -55,10 +66,10 @@ impl Editor {
     /// * `self` - The editor in which to move the cursor.
     /// * `num_columns` - The number of columns to move the cursor right.
     pub fn move_cursor_right(&mut self, num_columns: u16) {
-        if self.column_ix + num_columns > self.columns {
+        if self.column_ix + num_columns > self.dimensions.columns {
             // Ensure cursor remains within editor bounds.
             // TODO: Handle horizontal scrolling of document if line is wider than editor.
-            self.column_ix = self.columns - 1;
+            self.column_ix = self.dimensions.columns - 1;
         } else {
             self.column_ix += num_columns;
         }
@@ -86,9 +97,9 @@ impl Editor {
     /// * `self` - The editor in which to move the cursor.
     /// * `num_rows` - The number of rows to move the cursor down.
     pub fn move_cursor_down(&mut self, num_rows: u16) {
-        if self.row_ix + num_rows > self.rows {
+        if self.row_ix + num_rows > self.dimensions.rows {
             // Ensure cursor remains within editor bounds.
-            self.row_ix = self.rows - 1;
+            self.row_ix = self.dimensions.rows - 1;
         } else {
             self.row_ix += num_rows;
         }
@@ -99,45 +110,68 @@ impl Editor {
 mod tests {
     use super::*;
 
-    /// Tests that the editor moves the cursor left one column when instructed.
+    /// Tests that the editor moves the cursor to the left.
+    /// 
+    /// Does not attempt to move the cursor outside the render boundary.
     #[test]
-    fn moves_cursor_left_one_column() {
-        let mut editor = Editor::new();
+    fn moves_cursor_left_within_bounds() {
+        let mut editor = Editor::new(Dimensions::new(80, 24));
 
-        // Set the dimensions of this editor
-        editor.resize(80, 24);
-
-        // Move the cursor 10 columns right
+        // Move the cursor 10 columns right, then 1 column to the left
         editor.move_cursor_right(10);
-
-        // Move cursor one column to the left
         editor.move_cursor_left(1);
 
-        // Verify that the cursor position moved as expected
+        // Verify that the cursor moved as expected
         assert_eq!(editor.column_ix, 9);
     }
 
-    /// Tests that the editor moves the cursor right one column when instructed.
+    /// Tests that the editor moves the cursor to the right.
+    /// 
+    /// Does not attempt to move the cursor outside the render boundary.
     #[test]
-    fn moves_cursor_right_one_column() {
-        let mut editor = Editor::new();
-
-        // Set the dimensions of this editor
-        editor.resize(80, 24);
+    fn moves_cursor_right_within_bounds() {
+        let mut editor = Editor::new(Dimensions::new(80, 24));
 
         // Move cursor one column to the right
         editor.move_cursor_right(1);
 
-        // Verify that the cursor position moved as expected
+        // Verify that the cursor moved as expected
         assert_eq!(editor.column_ix, 1);
     }
 
+    /// Tests that the editor moves the cursor up.
+    /// 
+    /// Does not attempt to move the cursor outside the render boundary.
     #[test]
-    fn constrains_cursor_within_editor_left_side() {
-        let mut editor = Editor::new();
+    fn moves_cursor_up_within_bounds() {
+        let mut editor = Editor::new(Dimensions::new(80,24));
 
-        // Set the dimensions of the editor
-        editor.resize(80, 24);
+        // Move the cursor down 3 rows then up 1 row
+        editor.move_cursor_down(3);
+        editor.move_cursor_up(1);
+
+        // Verify that the cursor moved as expected
+        assert_eq!(editor.row_ix, 2);
+    }
+
+    /// Tests that the editor moves the cursor down.
+    /// 
+    /// Does not attempt to move the cursor outside the render boundary.
+    #[test]
+    fn moves_cursor_down_within_bounds() {
+        let mut editor = Editor::new(Dimensions::new(80,24));
+
+        // Move the cursor down 1 row
+        editor.move_cursor_down(1);
+
+        // Verify that the cursor moved as expected
+        assert_eq!(editor.row_ix, 1);
+    }
+
+    /// Cursor should be constrained by the left side of the render boundary.
+    #[test]
+    fn cursor_constrained_within_editor_left_side() {
+        let mut editor = Editor::new(Dimensions::new(80, 24));
 
         // Attempt to move the cursor left (should constrain to 0)
         editor.move_cursor_left(1);
@@ -145,12 +179,10 @@ mod tests {
         assert_eq!(editor.column_ix, 0);
     }
 
+    /// Cursor should be constrained by the right side of the render boundary.
     #[test]
     fn constrains_cursor_within_editor_right_side() {
-        let mut editor = Editor::new();
-
-        // Set the dimensions of the editor
-        editor.resize(80, 24);
+        let mut editor = Editor::new(Dimensions::new(80, 24));
 
         // Attempt to move the cursor right (should constrain to editor width)
         editor.move_cursor_right(100);
