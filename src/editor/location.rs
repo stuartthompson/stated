7477dbbd4d903af80aa597ed19da1963483de2d8
Stuart Thompson <stuart@stuartthompson.net>
2021-08-02 16:42:33 -0700

    Location: Struct used to track editor locations.

---
+++ src/editor/location.rs
@@ -0,0 +1,17 @@
+pub struct Location {
+    /// The index of the column described by this location.
+    ///
+    /// Zero-based. The 0th column is the left-hand most column.
+    pub column_ix: u16,
+
+    /// The index of the row described by this location.
+    ///
+    /// Zero-based. The 0th row is the top row in the editor.
+    pub row_ix: u16,
+}
+
+impl Location {
+    pub fn default() -> Location {
+        Location { column_ix: 0, row_ix: 0 }
+    }
+}
